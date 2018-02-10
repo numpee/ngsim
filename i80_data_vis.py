92eec7d13b7f7ba7e4a662c936e192826575a6c6
dongwan123 <dongwan123@gmail.com>
2018-02-11 02:47:24 +0900

    moving rectangles and angles

--- i80_data_vis.py
+++ i80_data_vis.py
@@ -3,15 +3,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import matplotlib.patches as patches
+import math
 
 # Units are in FEET!
 
 filepath = 'trajectories-0400-0415.csv'
 data = pd.read_csv(filepath)
-data_cut = data[['Vehicle_ID', 'Frame_ID', 'Local_X', 'Local_Y']]
+data_cut = data[['Vehicle_ID', 'Frame_ID', 'Local_X', 'Local_Y','Lane_ID','Angle']]
 sorted_frame = data_cut.sort_values(by=['Frame_ID'])
 sorted_np = sorted_frame.values
-sorted_np = sorted_np[0:30000,:]
+sorted_np = sorted_np[1000:30000,:]
 
 # init array of sliced values, by frame number
 sliced = []
@@ -22,10 +23,10 @@ for i in range(int(min(sorted_np[:,1])),int(max(sorted_np[:,1]))):
 
 #fig, ax = plt.subplots()
 img = plt.imread("ASPeachtree.jpg")
-fig = plt.figure(figsize=(18,7))
+fig = plt.figure(figsize=(18,6))
 #ax = fig.add_axes([0,0,1,1],frameon=False)
-ax = fig.add_subplot(2,1,1)
-ax1 = fig.add_subplot(2,1,2)
+# ax = fig.add_subplot(2,1,2)
+ax1 = fig.add_subplot(1,1,1)
 #fig, ax = plt.subplots()
 
 # patch = patches.Rectangle((0, 0), 0, 0, fc='y', angle=30)
@@ -34,30 +35,41 @@ ax1 = fig.add_subplot(2,1,2)
 def animate(i):
     x = sliced[i][:,2]
     y = sliced[i][:,3]
+    angles = sliced[i][:,5]
     names = sliced[i][:,0]
-    ax.clear()
+    # ax.clear()
     ax1.clear()
     #ax.imshow(img, extent = [-300,300,0,1500])
-    ax.set_autoscaley_on(False)
-    ax.set_autoscalex_on(False)
-    ax.set_xlim([0,1600])
-    ax.set_ylim([0,100])
-    ax.scatter(y,x, s = 10)
+    # ax.set_autoscaley_on(False)
+    # ax.set_autoscalex_on(False)
+    # ax.set_xlim([200,330])
+    # ax.set_ylim([0,100])
+    # ax.scatter(y,x, s = 10)
 
     ax1.set_autoscaley_on(False)
     ax1.set_autoscalex_on(False)
-    ax1.set_xlim([0,600])
-    ax1.set_ylim([0,70])
-    ax1.scatter(y,x,s=10)
+    ax1.set_xlim([70,370])
+    ax1.set_ylim([0,100])
+    # ax1.scatter(y,x,s=10)
     patches = []
+    patches1 = []
     # ax1.scatter(y,x, s = 50, marker = "s")
-    for x_cent, y_cent in zip(x,y):
-        print(x_cent, y_cent)
-        patches.append(ax1.add_patch(plt.Rectangle((y_cent, x_cent), 4, 2, fill=False, edgecolor="blue")))
+    for x_cent, y_cent,car_angles in zip(x,y,angles):
+        print(x_cent, y_cent, car_angles)
+        if car_angles<30 and car_angles>-30:
+            patches.append(ax1.add_patch(plt.Rectangle((y_cent, x_cent), 3, 2, fill=False, edgecolor="blue",angle=car_angles)))
+        else:
+            patches.append(ax1.add_patch(plt.Rectangle((y_cent, x_cent), 3, 2, fill=False, edgecolor="blue",angle=0)))
 
-    for i, txt in enumerate(names):
-         ax1.annotate(int(txt), (int(y[i]),int(x[i])), fontsize=10)
+        #patches1.append(ax.add_patch(plt.Rectangle((y_cent, x_cent), 3, 2, fill=False, edgecolor="blue",label=lane_label)))
+    # for i, txt in enumerate(names):
+    #      ax1.annotate(int(txt), (int(y[i]),int(x[i])), fontsize=10)
+    # if i%2==0:
+    #     return patches
+    # else:
+    #     return patches1
     return patches
+
 
 # transcribed at 100ms intervalS
 ani = animation.FuncAnimation(fig, animate, frames = range(2,30000), interval=100, blit=True)
