f2b6924b903ffbb6d49874ca4441c7d5cc34e3a1
dongwan123 <dongwan123@gmail.com>
2018-02-12 01:15:33 +0900

    final without lanes

--- i80_data_vis.py
+++ i80_data_vis.py
@@ -9,7 +9,7 @@ import math
 
 filepath = 'trajectories-0400-0415.csv'
 data = pd.read_csv(filepath)
-data_cut = data[['Vehicle_ID', 'Frame_ID', 'Local_X', 'Local_Y','Lane_ID','Angle']]
+data_cut = data[['Vehicle_ID', 'Frame_ID', 'Local_X', 'Local_Y','Lane_ID','Angle', 'v_Length', 'v_Width']]
 sorted_frame = data_cut.sort_values(by=['Frame_ID'])
 sorted_np = sorted_frame.values
 sorted_np = sorted_np[1000:30000,:]
@@ -37,6 +37,10 @@ def animate(i):
     y = sliced[i][:,3]
     angles = sliced[i][:,5]
     names = sliced[i][:,0]
+    lane_label = sliced[i][:,4]
+    vehicle_length = sliced[i][:,6]
+    vehicle_width = sliced[i][:,7]
+
     # ax.clear()
     ax1.clear()
     #ax.imshow(img, extent = [-300,300,0,1500])
@@ -48,18 +52,21 @@ def animate(i):
 
     ax1.set_autoscaley_on(False)
     ax1.set_autoscalex_on(False)
-    ax1.set_xlim([70,370])
+    ax1.set_xlim([200,400])
     ax1.set_ylim([0,100])
     # ax1.scatter(y,x,s=10)
     patches = []
     patches1 = []
+    lane_color = ["white", "red", "orange", "yellow", "green", "blue", "black", "pink"]
     # ax1.scatter(y,x, s = 50, marker = "s")
-    for x_cent, y_cent,car_angles in zip(x,y,angles):
+    for x_cent, y_cent,car_angles, lane, vlength, vwidth in zip(x,y,angles,lane_label,vehicle_length, vehicle_width):
         print(x_cent, y_cent, car_angles)
-        if car_angles<30 and car_angles>-30:
-            patches.append(ax1.add_patch(plt.Rectangle((y_cent, x_cent), 3, 2, fill=False, edgecolor="blue",angle=car_angles)))
+        if car_angles<25 and car_angles>-25:
+            patches.append(ax1.add_patch(plt.Rectangle((y_cent-vlength/2, x_cent-vwidth/2), vlength, vwidth,
+                        fill=False, angle=car_angles, linewidth = 2, edgecolor = lane_color[int(lane)])))
         else:
-            patches.append(ax1.add_patch(plt.Rectangle((y_cent, x_cent), 3, 2, fill=False, edgecolor="blue",angle=0)))
+            patches.append(ax1.add_patch(plt.Rectangle((y_cent-vlength/2, x_cent-vwidth/2), vlength, vwidth,
+                        fill=False, angle=0, linewidth = 2, edgecolor = lane_color[int(lane)])))
 
         #patches1.append(ax.add_patch(plt.Rectangle((y_cent, x_cent), 3, 2, fill=False, edgecolor="blue",label=lane_label)))
     # for i, txt in enumerate(names):
